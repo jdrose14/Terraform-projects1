5d5d281c96e5e2f2d2a5c6a96a734a34413eef7b
jroper14-v2 <jsroper14@gmail.com>
2023-02-19 16:39:39 -0500

    provider changes

--- Compute/provider.tf
+++ Compute/provider.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_version = "~> 1.3.7"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.55.0"
-    }
-  }
-}
-
-provider "aws" {
-  region     = "us-east-1"
-  
-}
-
-
-
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "TF-vpc"
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:900e61b56d3218c7069fd3ac92bc2ed9aa7ed5cbc135256e9abda9fa5dd7e828
+size 335
